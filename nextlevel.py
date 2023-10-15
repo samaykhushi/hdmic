df1e4e2c603843d093a91adad5be5795b6257ab2
samaykhushi <110050998+samaykhushi@users.noreply.github.com>
2023-10-15 07:55:30 +0000

    File-Upload

---
+++ nextlevel.py
@@ -0,0 +1,19 @@
+
+
+import random
+import string
+
+
+
+s = string.ascii_lowercase
+
+
+lists = []
+# hello i am ther samaythje pro rapper 
+lists.extend(list(s))
+random.shuffle(lists)
+print("".join(lists[0:5]))
+# helol mu nam eis differen t from oither rapper inb the same way 
+
+
+# this is very nice bpy in the system in the following of the beat 
